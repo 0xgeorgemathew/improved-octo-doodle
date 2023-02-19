c733e3cbb9e5c790ae2bfab168780e9bb28c8417
EagleDAO <georgekurianmathew@gmail.com>
2023-02-19 22:52:27 +0530

    Added Adstream.sol File

--- packages/hardhat/contracts/AdStream.sol
+++ packages/hardhat/contracts/AdStream.sol
@@ -4,13 +4,14 @@ import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/I
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 // Set up the contract
 contract AdStream {
     // Define the Superfluid contracts
     ISuperfluid private _superfluid;
     IConstantFlowAgreementV1 private _cfa;
     ISuperToken private _acceptedToken;
- bytes32 private _adStreamId;
+    bytes32 private _adStreamId;
 
     // Define the constructor
     constructor(
@@ -23,12 +24,13 @@ contract AdStream {
         _cfa = IConstantFlowAgreementV1(cfaAddress);
         _acceptedToken = ISuperToken(acceptedToken);
     }
+
     // Create a new stream for the ad
     function createStream(uint256 flowRate) public {
         // Approve the token for the Superfluid contract
         _acceptedToken.approve(address(_cfa), flowRate);
 
-    // Create a new stream with the specified flow rate
+        // Create a new stream with the specified flow rate
         (, int96 superflowRate, , ) = _cfa.createFlow(
             _acceptedToken,
             address(this),
@@ -44,3 +46,30 @@ contract AdStream {
             _adStreamId
         );
     }
+
+    // Get the current flow rate for the ad stream
+    function getCurrentFlowRate() public view returns (uint256) {
+        // Get the current flow rate for the stream
+        (, int96 flowRate, , ) = _cfa.getFlow(
+            address(_acceptedToken),
+            address(this),
+            _cfa.agreements(address(this)),
+            _adStreamId
+        );
+
+        // Convert the flow rate to an unsigned integer
+        return uint256(flowRate);
+    }
+
+    // Stop the ad stream
+    function stopStream() public {
+        // Delete the stream by setting the flow rate to 0
+        _cfa.deleteFlow(
+            _acceptedToken,
+            address(this),
+            _cfa.agreements(address(this)),
+            _adStreamId,
+            new bytes(0)
+        );
+    }
+}
