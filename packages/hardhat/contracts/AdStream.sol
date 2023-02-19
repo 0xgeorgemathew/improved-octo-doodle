ad34b0e21fc3122db2ed9783034b57d16a411ac3
EagleDAO <georgekurianmathew@gmail.com>
2023-02-19 22:45:58 +0530

    Added Adstream.sol File

---
+++ packages/hardhat/contracts/AdStream.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Import the required Superfluid and Solidity contract interfaces
+import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
